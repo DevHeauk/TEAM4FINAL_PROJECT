00807dafbee8145b9b44b4867c340116f3604aed
yuheaukjun <33533349+yuheaukjun@users.noreply.github.com>
2018-02-05 14:29:34 +0900

    etc..
    etc..

--- project/src/main/webapp/WEB-INF/table.sql
+++ project/src/main/webapp/WEB-INF/table.sql
@@ -1,3 +1,44 @@
+create table team(
+   name VARCHAR2(100),
+   member VARCHAR2(100),
+   win number,
+   lose number,
+   total number,
+   ground VARCHAR2(100),
+   leader VARCHAR2(100)
+   
+)
+
+create table joinlist(
+   jointeam varchar2(100)
+   joinid varchar2(100)
+)
+
+CREATE TABLE matching (
+hometeam varchar2(200),
+homepoint number,
+awayteam varchar2(200),
+awaypoint number,
+matchdate varchar2(200),
+ground varchar2(200),
+successmatching varchar2(100)
+)
+
+CREATE TABLE SHOP4(
+num NUMBER,
+writer VARCHAR2(100),
+title VARCHAR2(100),
+client_ID VARCHAR2(100),
+price NUMBER, 
+content CLOB,
+remainCount NUMBER,
+orgfilename VARCHAR2(100),
+savefilename VARCHAR2(100),
+filesize NUMBER,
+product_size VARCHAR2(100),
+regdate DATE,
+category VARCHAR2(100))
+
 CREATE TABLE users4(
 num NUMBER PRIMARY KEY,
 name VARCHAR2(100),
